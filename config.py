94a3cc6674a1e7b83ce39e7429b172a3f0f0c137
Gustavo Murilo <murilogustavo.go@gmail.com>
2025-11-07 12:40:30 -0300

    AJUSTES DE AMBIENTE

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SECRET_KEY = 'NET_CARS'
 DEBUG = True
 DB_HOST = 'localhost'
-DB_NAME = r'C:\Users\felip\OneDrive\Documentos\GitHub\Netcars_API\upload\banco\NETCARS.FDB'
+DB_NAME = r'C:\Users\muril\OneDrive\Documentos\GitHub\CarHub-API\upload\banco\NETCARS.FDB'
 
 DB_USER = 'SYSDBA'
 DB_PASSWORD = 'sysdba'
